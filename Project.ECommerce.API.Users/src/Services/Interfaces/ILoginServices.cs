74db50d144f4393b932e42c09fb392620341d98d
Gustavo <gcostacoelho2003@gmail.com>
2024-03-09 11:15:05 -0300

    feat: created a methods to get a loginInfos and updated a email field

--- Project.ECommerce.API.Users/src/Services/Interfaces/ILoginServices.cs
+++ Project.ECommerce.API.Users/src/Services/Interfaces/ILoginServices.cs
@@ -1,7 +1,12 @@
+using Project.ECommerce.API.Users.src.Models.Login;
 using Project.ECommerce.API.Users.src.Models.Utils;
 
 namespace Project.ECommerce.API.Users.src.Services.Interfaces;
 public interface ILoginServices
 {
-    Task<ApiResponse<string>> UpdatePasswordAsync(string userId, string newPass);
+    Task<ApiResponse<string>> UpdatePasswordAsync(string email, string newPass);
+
+    Task<ApiResponse<string>> UpdateEmailAsync(string email, string newEmail);
+
+    Task<ApiResponse<LoginInfos>> GetLoginsInfosAsync(string  email);
 }
